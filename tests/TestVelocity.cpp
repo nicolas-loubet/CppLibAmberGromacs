f21fc6fa387403233e3dc07850dd54cf48784d0f
Nicolás Loubet <loubetnicolasalfredo@gmail.com>
2025-06-15 10:38:26 -0300

    Simplifico el esquema de importación con un único archivo de entrada.

--- tests/TestVelocity.cpp
+++ tests/TestVelocity.cpp
@@ -1,5 +1,4 @@
-#include "../cppambergromacs/ReaderFactory.hpp"
-#include "../cppambergromacs/Configurations/Configuration.hpp"
+#include "../cppambergromacs/CppLibAmberGromacs.hpp"
 #include <iostream>
 using namespace std;
 
@@ -8,7 +7,7 @@ int main() {
     vector<pair<int,string>> files= CoordinateReader::getFileIterator("../files/", "amber_frame_*.pdb");
     string f= "../files/"+files[0].second;
     ToolKit::takeTime([&ti,&f]() {
-        for(int i= 0; i < 1000; i++) {
+        for(int i= 0; i < 100; i++) {
             Configuration conf= Configuration(ReaderFactory::createCoordinateReader(ReaderFactory::ProgramFormat::AMBER), f, ti);
         }
     });
