5f9411fb88a466c9dfe4e1111178e63fe461209a
Nicolás Loubet <loubetnicolasalfredo@gmail.com>
2025-06-14 21:43:39 -0300

    Traducción completa, eliminación de .hpp auxiliar, agregado de versión.

--- tests/TestVelocity.cpp
+++ tests/TestVelocity.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 int main() {
-    TopolInfo ti= ReaderFactory::createTopologyReader(ReaderFactory::ProgramFormat::AMBER)->readTopology("../archivos/jj.prmtop");
-    vector<pair<int,string>> files= CoordinateReader::getFileIterator("../archivos/", "frame_*.pdb");
-    string f= "../archivos/"+files[0].second;
+    TopolInfo ti= ReaderFactory::createTopologyReader(ReaderFactory::ProgramFormat::AMBER)->readTopology("../files/amber.prmtop");
+    vector<pair<int,string>> files= CoordinateReader::getFileIterator("../files/", "amber_frame_*.pdb");
+    string f= "../files/"+files[0].second;
     ToolKit::takeTime([&ti,&f]() {
         for(int i= 0; i < 1000; i++) {
             Configuration conf= Configuration(ReaderFactory::createCoordinateReader(ReaderFactory::ProgramFormat::AMBER), f, ti);
