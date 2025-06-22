4c949bd89c2c92eac6fd5d5650a62e7e8d22b879
Nicolás Loubet <loubetnicolasalfredo@gmail.com>
2025-06-22 00:29:50 -0300

    Agrego TERs y reviso errores, sigue fallando.

--- cppambergromacs/AmberReaders.hpp
+++ cppambergromacs/AmberReaders.hpp
@@ -44,7 +44,6 @@ class AmberTopologyReader : public TopologyReader {
             int _i = 0;
             file_prmtop.clear();
             file_prmtop.seekg(position);
-            cout <<position<<endl;
 
             while (getline(file_prmtop, line)) {
                 if (line.find("%FLAG POINTERS") != string::npos) {
@@ -61,6 +60,7 @@ class AmberTopologyReader : public TopologyReader {
                     stringstream ss(line);
                     string this_value;
                     while (ss >> this_value) {
+                        cout << "dict_pointers[" << list_of_names[_i] << "]: " << this_value << endl;
                         dict_pointers[list_of_names[_i]] = stoi(this_value);
                         _i++;
                     }
@@ -501,20 +501,15 @@ class AmberTopologyReader : public TopologyReader {
             file.clear();
             file.seekg(position);
             while (getline(file, line)) {
-                
                 if (line.find("%FLAG MASS") != string::npos) {
                     inFlag = true;
                     continue;
                 }
-
                 if (inFlag) {
                     if (line.find("%FORMAT") != string::npos) {
                         // Saltamos la línea del formato
                         continue;
                     }
-                    stringstream ss(line);
-                    
-                    
                     for (size_t i = 0; i + 15 < line.length(); i += 16) {
                         mass[_j]=stof(ToolKit::strip(line.substr(i, 16)));
                         ++_j;
@@ -557,9 +552,6 @@ class AmberTopologyReader : public TopologyReader {
                         continue;
                     }
                     
-                    // Read and split variable values
-                    stringstream ss(line);
-                    
                     for (size_t i = 0; i + 15< line.length(); i += 16) {
                         float coef_a=stof(ToolKit::strip(line.substr(i, 16)));
                         
@@ -591,9 +583,6 @@ class AmberTopologyReader : public TopologyReader {
                         // Skipping format line
                         continue;
                     }
-                    
-                    // Read and split variable values
-                    stringstream ss(line);
                     
                     for (size_t i = 0; i + 15< line.length(); i += 16) {
                         float coef_b=stof(ToolKit::strip(line.substr(i, 16)));
@@ -671,21 +660,21 @@ class AmberTopologyReader : public TopologyReader {
 
             ifstream file(filename);
 
-            map<string, int> positions_of_flags=flag_position(file);
-            map<string,int> dict_pointers = read_pointers(file,positions_of_flags["POINTERS"]);
-            vector<string> atom_names = read_atom_name(file, dict_pointers,positions_of_flags["ATOM_NAME"]);
-            vector<tuple<string,float>> map_atoms = read_charge(file, dict_pointers, atom_names ,positions_of_flags["CHARGE"]);
-            vector<int> number_solute_solvent =read_solvent_pointers(file, dict_pointers,positions_of_flags["SOLVENT_POINTERS"]);
-            vector<string> ati_to_amber_type=atom_type_index_to_amber_type(file,  dict_pointers , positions_of_flags);        
-            vector<int> atomic_number=read_atomic_number(file,dict_pointers,positions_of_flags["ATOMIC_NUMBER"]); 
-            map<string,tuple<int,int>> atoms_per_diff_molecule=read_atoms_per_different_molecule(file, positions_of_flags);
-            vector<int> atoms_per_molecule = read_atoms_per_molecule(file, dict_pointers,positions_of_flags["SOLVENT_POINTERS"]); 
-            vector<int> atom_type_index=read_ati(file,dict_pointers,positions_of_flags["ATOM_TYPE_INDEX"]); 
-            vector<float> mass=read_mass(file,dict_pointers,positions_of_flags["MASS"]);
-            map<pair<string,string>,pair<float,float>> lj_coefficient=read_lj(file,dict_pointers,positions_of_flags["LENNARD_JONES_ACOEF"],  ati_to_amber_type);
-            map<string,pair<float,float>> lj_diagonal= read_lj_diagonal(lj_coefficient);
-            map<string,int> type_atomic_z = type_atomic_number(dict_pointers, atom_type_index,atomic_number,ati_to_amber_type);
-            map<string,string> name_types = read_name_type(dict_pointers, atom_names,atom_type_index,ati_to_amber_type);
+            cout << "!" << endl; map<string, int> positions_of_flags=flag_position(file);
+            cout << "!" << endl; map<string,int> dict_pointers = read_pointers(file,positions_of_flags["POINTERS"]);
+            cout << "!" << endl; vector<string> atom_names = read_atom_name(file, dict_pointers,positions_of_flags["ATOM_NAME"]);
+            cout << "!" << endl; vector<tuple<string,float>> map_atoms = read_charge(file, dict_pointers, atom_names ,positions_of_flags["CHARGE"]);
+            cout << "!" << endl; vector<int> number_solute_solvent =read_solvent_pointers(file, dict_pointers,positions_of_flags["SOLVENT_POINTERS"]);
+            cout << "!" << endl; vector<string> ati_to_amber_type=atom_type_index_to_amber_type(file,  dict_pointers , positions_of_flags);        
+            cout << "!" << endl; vector<int> atomic_number=read_atomic_number(file,dict_pointers,positions_of_flags["ATOMIC_NUMBER"]); 
+            cout << "!" << endl; map<string,tuple<int,int>> atoms_per_diff_molecule=read_atoms_per_different_molecule(file, positions_of_flags);
+            cout << "!" << endl; vector<int> atoms_per_molecule = read_atoms_per_molecule(file, dict_pointers,positions_of_flags["SOLVENT_POINTERS"]); 
+            cout << "!" << endl; vector<int> atom_type_index=read_ati(file,dict_pointers,positions_of_flags["ATOM_TYPE_INDEX"]); 
+            cout << "!" << endl; vector<float> mass=read_mass(file,dict_pointers,positions_of_flags["MASS"]);
+            cout << "!" << endl; map<pair<string,string>,pair<float,float>> lj_coefficient=read_lj(file,dict_pointers,positions_of_flags["LENNARD_JONES_ACOEF"],  ati_to_amber_type);
+            cout << "!" << endl; map<string,pair<float,float>> lj_diagonal= read_lj_diagonal(lj_coefficient);
+            cout << "!" << endl; map<string,int> type_atomic_z = type_atomic_number(dict_pointers, atom_type_index,atomic_number,ati_to_amber_type);
+            cout << "!" << endl; map<string,string> name_types = read_name_type(dict_pointers, atom_names,atom_type_index,ati_to_amber_type);
             
             topology.num_molecules=number_solute_solvent[1];
 			topology.num_solutes=number_solute_solvent[0];
@@ -827,7 +816,7 @@ class AmberCoordinateReader : public CoordinateReader {
             getline(f,line);
             if(line.rfind("CRYST1",0) == 0)
                 bounds= Vector( stof(line.substr(6,9)) , stof(line.substr(15,9)) , stof(line.substr(24,9)) );
-            cout<<topol_info.num_molecules<<endl;
+            
             for(int i= 0; i < topol_info.num_molecules; i++)
             {
                 const auto& atom_data= topol_info.atom_type_name_charge_mass[i];
