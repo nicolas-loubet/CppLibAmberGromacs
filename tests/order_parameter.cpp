d2907d065db9369dfe29127bf8063372dbd91882
Victor Ezequiel Cuenca <huevo_x@yahoo.com>
2025-07-18 16:59:12 -0300

    Calculo de parametro de orden

--- tests/order_parameter.cpp
+++ tests/order_parameter.cpp
@@ -2,27 +2,45 @@
 #include "../cppambergromacs/CppLibAmberGromacs.hpp"
 
 int main() {
-    cout<<"0"<<endl;
-    TopolInfo ti= ReaderFactory::createTopologyReader(ReaderFactory::ProgramFormat::AMBER)->readTopology("step5_input.parm7");
-    cout<<"1"<<endl;
+
+    TopolInfo ti1= ReaderFactory::createTopologyReader(ReaderFactory::ProgramFormat::AMBER)->readTopology("step5_input.parm7");
     CoordinateReader* cr= ReaderFactory::createCoordinateReader(ReaderFactory::ProgramFormat::AMBER);
-    cout<<"2"<<endl;
-    ConfigurationLipid c= ConfigurationLipid(cr, "frame1.pdb", ti);
-    cout<<"3"<<endl;
+    ConfigurationLipid c1= ConfigurationLipid(cr, "frame1.pdb", ti1);
+
+    TopolInfo ti2= ReaderFactory::createTopologyReader(ReaderFactory::ProgramFormat::AMBER)->readTopology("DMPC/jj.prmtop");
+    ConfigurationLipid c2= ConfigurationLipid(cr, "DMPC/frame1.pdb", ti2);
     
-    vector<float> resultado(20);
-    for(int i=1; i<=ti.num_solutes;i++)
+    vector<float> resultado1(23);
+    vector<float> resultado2(23);
+
+    for(int i=1; i<=ti1.num_solutes;i++)
         {
-        vector<float> resultado_molecula = c.orderParameter(i);
+        vector<float> resultado_molecula = c1.orderParameter(i);
         for(int j=0; j<resultado_molecula.size();j++)
             {
-            resultado[j]+=resultado_molecula[j]/ti.num_solutes;
+            resultado1[j]+=resultado_molecula[j]/ti1.num_solutes;
             }
         }
-    for(int i=0; i<resultado.size();i++)
+    for(int i=1; i<=ti2.num_solutes;i++)
         {
-        cout<<"Atom C: "<<i<<" Order parameter: "<<resultado[i]<<endl;
+        vector<float> resultado_molecula = c2.orderParameter(i);
+        for(int j=0; j<resultado_molecula.size();j++)
+            {
+            resultado2[j]+=resultado_molecula[j]/ti2.num_solutes;
+            }
         }
-    cout<<"4"<<endl;
+
+    ofstream f("parametro_de_orden.csv");
+    f<<"Carbono,"<< "Orden_gel,"<<"Orden_cristal" <<endl; 
+    for(int i=0; i<resultado1.size();i++)
+        {
+        //cout<<"Atom C: "<<i<<" Order parameter: "<<resultado1[i]<<endl;
+        if(resultado1[i]!=0 || resultado2[i]!=0)
+            {
+            f<<i+1<<","<< resultado1[i] << "," << resultado2[i] << endl; 
+            }
+        }
+
+    f.flush(); f.close();
     return 0;
 }
