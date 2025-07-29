a3f7fa33593147bb8da3dc11df1b5f045e8105d4
Nicolás Loubet <loubetnicolasalfredo@gmail.com>
2025-07-29 12:22:40 -0300

    Definición

--- cppambergromacs/CppLibAmberGromacs.hpp
+++ cppambergromacs/CppLibAmberGromacs.hpp
@@ -1,6 +1,12 @@
 #ifndef CPPLIBAMBERGROMACS_HPP
 #define CPPLIBAMBERGROMACS_HPP
 
+#ifdef USE_DOUBLE_PRECISION
+    using Real= double;
+#else
+    using Real= float;
+#endif
+
 #include "ReaderFactory.hpp"
 #include "Configurations/ConfigurationBulk.hpp"
 #include "Configurations/ConfigurationLipid.hpp"
